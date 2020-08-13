b9c91bc980f8106a4b7f836659bcfb4be5756079
Milosz Gawel <mgawel@gmail.com>
2020-08-13 22:57:06 +0200

    Updates

---
+++ precache-manifest.f7a26c0dbdb0141b6b84d6c99de5e650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f681aad3922b441451cd20b6d8ad03a",
+    "url": "/PokemonWiki/index.html"
+  },
+  {
+    "revision": "271f4b48375d5ac3f75d",
+    "url": "/PokemonWiki/static/js/2.c6d77f05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PokemonWiki/static/js/2.c6d77f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8f949418478adee1fb",
+    "url": "/PokemonWiki/static/js/main.60e80453.chunk.js"
+  },
+  {
+    "revision": "2ebec2e59121b9ae9dd3",
+    "url": "/PokemonWiki/static/js/runtime-main.7538c852.js"
+  },
+  {
+    "revision": "6f92eb41dc3629894c83df35d6f7be7d",
+    "url": "/PokemonWiki/static/media/image.6f92eb41.png"
+  },
+  {
+    "revision": "95f96396af008beaecc652bb7ac6c322",
+    "url": "/PokemonWiki/static/media/logo.95f96396.png"
+  }
+]);
